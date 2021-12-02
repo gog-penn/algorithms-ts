7e961b3004e9ceb6d9f7502883e7aba0fd7828b6
gog <sokumelon-.-@sokumelons-MacBook-Pro.local>
2021-12-02 09:21:22 +0000

    linear search

--- src/search/linear/linearSearch.ts
+++ src/search/linear/linearSearch.ts
@@ -1,6 +1,14 @@
 import { NO_MATCH } from "../common";
 
 const linearSearch = (items: any[], item: any): number => {
+    
+    for (let index = 0; index < items.length; index++) {
+        if (items[index] === item) {
+            return index;
+            
+        }
+        
+    }
 
     return NO_MATCH;
 }
